d46b7c3e65645a0fd4c562148292a380aca258cb
Michael Kubiak <mk35@sanger.ac.uk>
2022-01-17 16:47:37 +0000

    Add ability to find places for non-illumina files

--- lost_and_found.py
+++ lost_and_found.py
@@ -24,27 +24,44 @@ def rm_or_keep(landf, existing, out):
         out.write(f"# md5 NOT OK, exists as {existing.path}/{existing.name}\n")
 
 
+def non_illumina(technology, obj):
+    path = obj.path
+    name = obj.name
+    real_path = f"/seq/{technology}{str(path).split(technology)[1]}"
+    match = DataObject(f"{real_path}/{name}")
+    with open("resolve_orphaned_files.sh", "a") as out:
+        if match.exists():
+            rm_or_keep(obj, match, out)
+        else:
+            logging.info("does not exist")
+            if Collection(real_path).exists():
+                logging.info(f"moving to {real_path}/{name}")
+                out.write(f"imv {path}/{name} {real_path}/{name} # {name} not present, runfolder at {real_path}\n")
+                return True
+            else:
+                logging.info(f"no {technology} runfolder for this run")
+                return False
+
+
 def main():
     logging.basicConfig(filename='orphan_files.log', encoding='utf-8', level=logging.INFO)
     lost_and_found = Collection("/seq/lostandfound")
 
-    with open("resolve_orphaned_files.sh", "w") as out:
-
-        for obj in lost_and_found.contents(recurse=True):
-            if type(obj) == DataObject:
-                path = obj.path
-                name = obj.name
-                technology = None
-                for t in technologies:
-                    if t in str(path).lower():
-                        technology = t
-                if technology == "illumina" or technology is None:
-                    cont = True
-                    # simple case - assume files are illumina and begin with run id
-                    id = name.split("_")[0]
-                    logging.info(f"{path}/{name}:")
-                    location_direct = f"/seq/{id}"
-                    location_illumina = f"/seq/illumina/runs/{id[:2]}/{id}"
+    for obj in lost_and_found.contents(recurse=True):
+        if type(obj) == DataObject:
+            path = obj.path
+            name = obj.name
+            technology = None
+            for t in technologies:
+                if t in str(path).lower():
+                    technology = t
+            if technology == "illumina" or technology is None:
+                found = True
+                id = name.split("_")[0]
+                logging.info(f"{path}/{name}:")
+                location_direct = f"/seq/{id}"
+                location_illumina = f"/seq/illumina/runs/{id[:2]}/{id}"
+                with open("resolve_orphaned_files.sh", "a") as out:
                     if DataObject(f"{location_direct}/{name}").exists():
                         rm_or_keep(obj, DataObject(f"{location_direct}/{name}"), out)
                     elif DataObject(f"{location_illumina}/{name}").exists():
@@ -60,29 +77,28 @@ def main():
                             else:
                                 pos = "illumina"
                         if pos == "direct":
-                            logging.info(f"not present, moving to {location_direct}")
+                            logging.info(f"moving to {location_direct}")
                             out.write(f"imv {path}/{name} {location_direct}/{name} # {name} not present, runfolder at {location_direct}\n")
                         elif pos == "illumina":
-                            logging.info(f"not present, moving to {location_illumina}")
+                            logging.info(f"moving to {location_illumina}")
                             out.write(f"imv {path}/{name} {location_illumina}/{name} # {name} not present, runfolder at {location_illumina}\n")
                         elif pos == "both":
                             logging.warning(f"Two run folders for run {id}")
                             out.write(f"# {name} not present, two possible runfolders for this run, {location_direct} and {location_illumina}\n")
                         else:
                             logging.info("no illumina runfolder for this run")
-                            cont = False
-                    if cont:
+                            found = False
+                if found:
+                    continue
+
+            for t in technologies:
+                if t == technology:
+                    if non_illumina(t, obj):
                         continue
-                if technology == "pacbio" or technology is None:
-                    ...
-                if technology == "ont" or technology is None:
-                    ...
-                if technology == "sequenom" or technology is None:
-                    ...
-                if technology == "fluidigm" or technology is None:
-                    ...
 
-
+            # if the file was not found, and there was no runfolder
+            with open ("still_lost.log", "a") as lost:
+                lost.write(f"A location for {path}/{name} has not been found")
 
 
 if __name__ == "__main__":
